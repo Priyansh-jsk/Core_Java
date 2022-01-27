c93a1b0dd30c72201940ac42dd46839e9d61b9ec
Priyansh <neemapriyanshrd786@gmail.com>
2022-01-27 16:16:01 +0530

    I have completed Assignment2 with following methods Circle, Cylinder, Rectanglr, Sphere, Square, Triangle

---
+++ src/Triangle.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Triangle {
+
+    public double area(double height, double base) {
+
+        try {
+            return (height * base) / 2;
+        }
+        catch (ArithmeticException arithmeticException){
+            arithmeticException.printStackTrace();
+            return 0.0;
+        }
+    }
+    public double perimeter(double height, double base, double side) {
+
+        try {
+            return height + base + side;
+        }
+        catch (ArithmeticException arithmeticException){
+            arithmeticException.printStackTrace();
+            return 0.0;
+        }
+    }
+}
