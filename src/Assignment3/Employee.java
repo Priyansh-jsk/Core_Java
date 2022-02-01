854e1945069318e7c7ed3ca961ff0c77c7ae1907
Priyansh <neemapriyanshrd786@gmail.com>
2022-02-01 17:53:32 +0530

    I have done changes in variable name in upper camel case and Indentation issue resolved

--- src/Assignment3/Employee.java
+++ src/Assignment3/Employee.java
@@ -1,5 +1,6 @@
 package Assignment3;
 
+import java.util.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -9,27 +10,31 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.ListIterator;
 import java.util.Scanner;
 
 class EmployeeDemo implements Serializable {
-	int empid;
-	String empname;
-	String empemail;
-	int empage;
-	String empDOB;
-	
-	EmployeeDemo(int empid, String empname, String empemail, int empage, String empDOB){
-		this.empid = empid;
-		this.empname = empname;
-		this.empemail = empemail;
-		this.empage = empage;
-		this.empDOB = empDOB;
+	int empId;
+	String empName;
+	String empEmail;
+	int empAge;
+	String empDob;
+
+	EmployeeDemo(int empId, String empName, String empEmail, int empAge, String empDob) {
+		this.empId = empId;
+		this.empName = empName;
+		this.empEmail = empEmail;
+		this.empAge = empAge;
+		this.empDob = empDob;
 	}
+
 	public String toString() {
-		return empid+" "+empname+" "+empemail+" "+empage+" "+empDOB;
+		return empId + " " + empName + " " + empEmail + " " + empAge + " " + empDob;
 	}
 }
+
 class Employee {
 	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
 		int choice = -1;
@@ -40,10 +45,10 @@ class Employee {
 		ObjectOutputStream oos = null;
 		ObjectInputStream ois = null;
 		ListIterator<EmployeeDemo> li = null;
-		
-		if(file.isFile()) {
+
+		if (file.isFile()) {
 			ois = new ObjectInputStream(new FileInputStream(file));
-			a1 = (ArrayList<EmployeeDemo>)ois.readObject();
+			a1 = (ArrayList<EmployeeDemo>) ois.readObject();
 			ois.close();
 		}
 		do {
@@ -52,108 +57,176 @@ class Employee {
 			System.out.println("3. Search Employee Data ");
 			System.out.println("4. Delete Employee Data ");
 			System.out.println("5. Update Employee Data");
+			System.out.println("6. Sort by EmployeeId Data");
+			System.out.println("7. Sort by EmployeeId in File");
 			System.out.println("0. Exit ");
 			System.out.println("Enter your Choice how many Employee you want to give: ");
 			choice = sc.nextInt();
-			
-			switch(choice) {
-			case 1:
+
+			switch (choice) {
+			case 1:                                                         //Add the details of Employee
 				System.out.println("Enter how many employee you want: ");
 				int n = sc.nextInt();
-				for(int i=0;i<n;i++) {
+				for (int i = 0; i < n; i++) {
 					System.out.print("Enter the Employee ID: ");
-					int empid = sc.nextInt();
-					
+					int empId = sc.nextInt();
+
 					System.out.print("Enter the Employee Name: ");
 					String empname = sc1.nextLine();
-					
+
 					System.out.print("Enter the Employee email: ");
-					String empemail = sc1.nextLine();
-					
+					String empEmail = sc1.nextLine();
+
 					System.out.print("Enter the Employee Age: ");
-					int empage = sc.nextInt();
-					
+					int empAge = sc.nextInt();
+
 					System.out.print("Enter the Employee DOB: ");
-					String empDOB = sc1.nextLine();
-					
-					a1.add(new EmployeeDemo(empid, empname, empemail, empage, empDOB));
+					String empDob = sc1.nextLine();
+
+					a1.add(new EmployeeDemo(empId, empname, empEmail, empAge, empDob));
 				}
 				oos = new ObjectOutputStream(new FileOutputStream(file));
 				oos.writeObject(a1);
 				oos.close();
-			break;
-			case 2:
-				if(file.isFile()) {
+				break;
+			case 2:                                                        //Display Employee Data
+				if (file.isFile()) {
 					ois = new ObjectInputStream(new FileInputStream(file));
-				    a1 = (ArrayList<EmployeeDemo>)ois.readObject();
-				    ois.close();
-				    System.out.println("------------------------------");
-				    li = a1.listIterator();
-				    while(li.hasNext())
-				    	System.out.println(li.next());
-				    System.out.println("------------------------------");
-				    }else {
-				    	System.out.println("File is not there..!!");
-				    }
-			break;
-			case 3:
-				if(file.isFile()) {
-					ois = new ObjectInputStream(new FileInputStream(file));
-					a1 = (ArrayList<EmployeeDemo>)ois.readObject();
+					a1 = (ArrayList<EmployeeDemo>) ois.readObject();
 					ois.close();
-					
+					System.out.println("------------------------------");
+					li = a1.listIterator();
+					while (li.hasNext())
+						System.out.println(li.next());
+					System.out.println("------------------------------");
+				} else {
+					System.out.println("File is not there..!!");
+				}
+				break;
+			case 3:                                                        //Search Employee Data
+				if (file.isFile()) {
+					ois = new ObjectInputStream(new FileInputStream(file));
+					a1 = (ArrayList<EmployeeDemo>) ois.readObject();
+					ois.close();
+
 					boolean found = false;
 					System.out.println("Enter empid to Search: ");
-				    int empid = sc.nextInt();
-				    System.out.println("------------------------------");
-				    li = a1.listIterator();
-				    while(li.hasNext()) {
-				    	EmployeeDemo ed = (EmployeeDemo)li.next();
-				    	if(ed.empid == empid) {
-				    		System.out.println(ed);
-				    		found = true;
-				    		}
-				    	}
-				    if(!found)
-				    	System.out.println("Please enter Vaid Record..!!");
-				    System.out.println("------------------------------");
-				}else {
+					int empId = sc.nextInt();
+					System.out.println("------------------------------");
+					li = a1.listIterator();
+					while (li.hasNext()) {
+						EmployeeDemo ed = (EmployeeDemo) li.next();
+						if (ed.empId == empId) {
+							System.out.println(ed);
+							found = true;
+						}
+					}
+					if (!found)
+						System.out.println("Please enter Vaid Record..!!");
+					System.out.println("------------------------------");
+				} else {
 					System.out.println("File is not there..!!");
 				}
-			break;
-			case 4:
-				if(file.isFile()) {
+				break;
+			case 4:  													//Delete Employee Data
+				if (file.isFile()) {
 					ois = new ObjectInputStream(new FileInputStream(file));
-					a1 = (ArrayList<EmployeeDemo>)ois.readObject();
+					a1 = (ArrayList<EmployeeDemo>) ois.readObject();
 					ois.close();
-					
+
 					boolean found = false;
 					System.out.print("Enter empid to Delete: ");
-				    int empid = sc.nextInt();
-				    System.out.println("------------------------------");
-				    li = a1.listIterator();
-				    while(li.hasNext()) {
-				    	EmployeeDemo ed = (EmployeeDemo)li.next();
-				    	if(ed.empid == empid) {
-				    		li.remove();
-				    		found = true;
-				    	}
-				    }
-				    if(found) {
-				    	oos = new ObjectOutputStream(new FileOutputStream(file));
-				        oos.writeObject(a1);
-				        oos.close();
-				    	System.out.println("Record deleted sucessfully..!!");
-				    }
-				    else { 
-				    	System.out.println("Please enter Vaid Record..!!");
-				    }
-				    System.out.println("------------------------------");
-				}else {
+					int empId = sc.nextInt();
+					System.out.println("------------------------------");
+					li = a1.listIterator();
+					while (li.hasNext()) {
+						EmployeeDemo ed = (EmployeeDemo) li.next();
+						if (ed.empId == empId) {
+							li.remove();
+							found = true;
+						}
+					}
+					if (found) {
+						oos = new ObjectOutputStream(new FileOutputStream(file));
+						oos.writeObject(a1);
+						oos.close();
+						System.out.println("Record deleted sucessfully..!!");
+					} else {			
+						System.out.println("Please enter Vaid Record..!!");
+					}
+					System.out.println("------------------------------");
+				} else {
 					System.out.println("File is not there..!!");
 				}
-			break;
-		 }
-		}while(choice!=0);
-	}	
+				break;
+			case 5:  												 	//Update Employee Data
+				if (file.isFile()) {
+					ois = new ObjectInputStream(new FileInputStream(file));
+					a1 = (ArrayList<EmployeeDemo>) ois.readObject();
+					ois.close();
+
+					Collections.sort(a1, new Comparator<EmployeeDemo>() {
+						public int compare(EmployeeDemo e1, EmployeeDemo e2) {
+							return e1.empId - e2.empId;
+						}
+					});
+					System.out.println("------------------------------");
+					li = a1.listIterator();
+					while (li.hasNext())
+						System.out.println(li.next());
+					System.out.println("------------------------------");
+				} else {
+					System.out.println("File is not there..!!");
+				}
+				break;
+			case 6:  													//Sort by EmployeeId Data
+				if (file.isFile()) {
+					ois = new ObjectInputStream(new FileInputStream(file));
+					a1 = (ArrayList<EmployeeDemo>) ois.readObject();
+					ois.close();
+
+					Collections.sort(a1, new Comparator<EmployeeDemo>() {
+						public int compare(EmployeeDemo e1, EmployeeDemo e2) {
+							return e1.empId - e2.empId;
+						}
+					});
+
+					System.out.println("------------------------------");
+					li = a1.listIterator();
+					while (li.hasNext())
+						System.out.println(li.next());
+					System.out.println("------------------------------");
+				} else {
+					System.out.println("File is not there..!!");
+				}
+				break;
+			case 7:  													//Sort by EmployeeId File
+				if (file.isFile()) {
+					ois = new ObjectInputStream(new FileInputStream(file));
+					a1 = (ArrayList<EmployeeDemo>) ois.readObject();
+					ois.close();
+
+					Collections.sort(a1, new Comparator<EmployeeDemo>() {
+						public int compare(EmployeeDemo e1, EmployeeDemo e2) {
+							return e1.empId - e2.empId;
+						}
+					});
+
+					oos = new ObjectOutputStream(new FileOutputStream(file));
+					oos.writeObject(a1);
+					oos.close();
+
+					System.out.println("------------------------------");
+					li = a1.listIterator();
+					while (li.hasNext())
+						System.out.println(li.next());
+					System.out.println("------------------------------");
+				} else {
+					System.out.println("File is not there..!!");
+				}
+				break;
+				      //Sort by EmployeeId in File
+			}
+		} while (choice != 0);
+	}
 }
