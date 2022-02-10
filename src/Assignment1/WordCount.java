59813a854a751860b608e880cd01c4fa507ebb78
Priyansh <neemapriyanshrd786@gmail.com>
2022-02-10 10:34:45 +0530

    Done proper indentation issues and put the file in my current direct..

--- src/Assignment1/WordCount.java
+++ src/Assignment1/WordCount.java
@@ -14,28 +14,28 @@ import java.util.TreeMap;
 
 public class WordCount {
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		Map<String, Integer> m1 = new HashMap<>(); // using HashMap
 
-        Map<String, Integer>m1 = new HashMap<>();
-
-        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Dell\\eclipse-workspace\\CoreJava\\src\\Assignment1\\Demodata.txt"))) {
-            StringBuilder sb = new StringBuilder();
-            String line;
-
-            while ((line = br.readLine()) != null) {
-            	if(m1.containsKey(line))
-            	{
-            		m1.put(line, m1.get(line)+1);
-                }
-            	else {
-            		m1.put(line, 1);
-            	}
-            }
-        }
-        List<Map.Entry <String, Integer>> rm = new ArrayList<>(m1.entrySet());
-        Collections.sort(rm, (o1, o2) -> o2.getValue() - o1.getValue());
-        for (Entry<String, Integer> pn: rm) {
-        	System.out.println("Name " + pn.getKey() + " " + pn.getValue());
-        }
-        }
-   }
+		try (BufferedReader br = new BufferedReader(new FileReader("Demodata.txt")))
+		{
+			StringBuilder sb = new StringBuilder();
+			String line;
+			
+			while ((line = br.readLine()) != null) {
+				if (m1.containsKey(line))
+				{
+					m1.put(line, m1.get(line) + 1);
+				}
+				else {
+					m1.put(line, 1);
+				}
+			}
+		}
+		List<Map.Entry<String, Integer>> rm = new ArrayList<>(m1.entrySet());
+		Collections.sort(rm, (o1, o2) -> o2.getValue() - o1.getValue());
+		for (Entry<String, Integer> pn : rm) {
+			System.out.println("Name " + pn.getKey() + " " + pn.getValue());
+		}
+	}
+}
